25eeb285ce186dff3f7331e6489d2a2a4d23bf28
Aamir Sohail <amirsohailali123@gmail.com>
2023-10-02 14:49:36 +0500

    Update Repo

--- script/download_ckpt.sh
+++ script/download_ckpt.sh
@@ -1,8 +1,6 @@
 # download aot-ckpt 
-gdown --id '1QoChMkTVxdYZ_eBlZhK2acq9KMQZccPJ' --output ./ckpt/R50_DeAOTL_PRE_YTB_DAV.pth
-
+wget -P ./ckpt https://github.com/USTAADCOM/Pro_Painter_Tool/releases/download/v1.1/R50_DeAOTL_PRE_YTB_DAV.pth
 # download sam-ckpt
-wget -P ./ckpt https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
-
+wget -P ./ckpt https://github.com/USTAADCOM/Pro_Painter_Tool/releases/download/v1.1/sam_vit_b_01ec64.pth
 # download grounding-dino ckpt
-wget -P ./ckpt https://huggingface.co/ShilongLiu/GroundingDINO/resolve/main/groundingdino_swint_ogc.pth
+wget -P ./ckpt https://github.com/USTAADCOM/Pro_Painter_Tool/releases/download/v1.1/groundingdino_swint_ogc.pth
